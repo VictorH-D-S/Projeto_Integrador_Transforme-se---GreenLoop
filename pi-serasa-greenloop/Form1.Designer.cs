4ae695b6b2f0c5c1839c66d23ecdfc0d0b8f0dd0
victor.gssilva9 <santossilvavictorgabriel@gmail.com>
2023-08-28 15:13:36 -0300

    Boão Home

--- pi-serasa-greenloop/Form1.Designer.cs
+++ pi-serasa-greenloop/Form1.Designer.cs
@@ -31,11 +31,20 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.wilBitButton2 = new WiLBiT.WiLBiTButton();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(205)))), ((int)(((byte)(50)))));
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.splitter1);
             this.panel1.Location = new System.Drawing.Point(-2, -2);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1905, 87);
@@ -56,6 +65,44 @@
             this.wilBitButton2.TabIndex = 2;
             this.wilBitButton2.UseVisualStyleBackColor = false;
             // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 87);
+            this.splitter1.TabIndex = 0;
+            this.splitter1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(679, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "HOME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(670, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "|";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(754, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "|";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -65,6 +112,8 @@
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -74,5 +123,9 @@
         private Panel panel1;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private WiLBiT.WiLBiTButton wilBitButton2;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Splitter splitter1;
     }
 }
