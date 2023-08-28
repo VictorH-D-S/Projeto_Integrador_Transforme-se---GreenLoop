50f78ce3bc387342aaf9eeee3d0489d1f46d735e
VICTOR HENRIQUE DOS SANTOS <victor.hsantos45@senacsp.edu.br>
2023-08-28 15:11:49 -0300

    Criei a classe de conexão e as outras classes do BD

---
+++ pi-serasa-greenloop/Conexao.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace pi-serasa-greenloop
+{
+    internal class Conexao
+    {
+        const string host = "Localhost", banco = "08_listas_tarefas", usuario = "root", senha = "",
+                     dadosConexao = $"Server={host};Database={banco};Uid={usuario};PwD={senha};";
+
+        static MySqlConnection conexao = new MySqlConnection(dadosConexao);
+
+        public static DataTable executaQuery(string query)
+        {
+
+            try
+            {
+                conexao.Open();
+
+                MySqlCommand comando = new MySqlCommand(query, conexao);
+                MySqlDataReader dados = comando.ExecuteReader();
+                DataTable tabela = new DataTable();
+                tabela.Load(dados);
+                return tabela;
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine("BAH PIÁ DEU ERRO TCHÊ");
+                Console.WriteLine(erro.Message);
+                return null;
+                throw;
+            }
+            finally { conexao.Close(); }
+        }
+    }
+}
---
+++ pi-serasa-greenloop/Registros.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Registros
+{
+	public Registros()
+	{
+
+	}
+}
