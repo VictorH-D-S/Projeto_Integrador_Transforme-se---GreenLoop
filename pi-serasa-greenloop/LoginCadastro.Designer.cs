43afe4e977538320edb3d6bfcf94f4083f6dfc98
murilosccp <murilosantosdonascimento1992@gmail.com>
2023-08-30 16:05:31 -0300

    alterei o nome do forms e painel

--- pi-serasa-greenloop/LoginCadastro.Designer.cs
+++ pi-serasa-greenloop/LoginCadastro.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace pi_serasa_greenloop
 {
-    partial class Form1
+    partial class LoginCadastro
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,28 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            button1 = new Button();
+            SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(315, 183);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            button1.Location = new Point(315, 183);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
---
+++ pi-serasa-greenloop/LoginCadastro.cs
@@ -0,0 +1,15 @@
+namespace pi_serasa_greenloop
+{
+    public partial class LoginCadastro : Form
+    {
+        void rebderizarInterface()
+        {
+           
+        }
+
+        public LoginCadastro()
+        {
+            InitializeComponent();
+        }
+    }
+}
