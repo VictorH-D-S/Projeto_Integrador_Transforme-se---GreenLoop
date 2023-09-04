4a241940a337e3e50b3e2df9be4929952cd3ac13
Isaquezak64 <135741800+Isaquezak64@users.noreply.github.com>
2023-09-04 18:04:10 -0300

    Form 1 corrigido

--- pi-serasa-greenloop/Form1.Designer.cs
+++ pi-serasa-greenloop/Form1.Designer.cs
@@ -28,21 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-
+            this.wilBitGradientPanel1 = new WiLBiT.WiLBiTGradientPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.painel = new System.Windows.Forms.Panel();
+            this.wilBitGradientPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // wilBitGradientPanel1
+            // 
+            this.wilBitGradientPanel1.BackColor = System.Drawing.Color.Green;
+            this.wilBitGradientPanel1.BackColor2 = System.Drawing.Color.LimeGreen;
+            this.wilBitGradientPanel1.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.wilBitGradientPanel1.BorderRadius = 6;
+            this.wilBitGradientPanel1.BorderSize = 0;
+            this.wilBitGradientPanel1.Controls.Add(this.label1);
+            this.wilBitGradientPanel1.ForeColor = System.Drawing.Color.White;
+            this.wilBitGradientPanel1.Location = new System.Drawing.Point(-4, -4);
+            this.wilBitGradientPanel1.Name = "wilBitGradientPanel1";
+            this.wilBitGradientPanel1.Size = new System.Drawing.Size(1919, 77);
+            this.wilBitGradientPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(825, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "GreenLoop";
+            // 
+            // painel
+            // 
+            this.painel.Location = new System.Drawing.Point(0, 70);
+            this.painel.Name = "painel";
+            this.painel.Size = new System.Drawing.Size(1918, 973);
+            this.painel.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(3)))), ((int)(((byte)(21)))));
             this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.painel);
+            this.Controls.Add(this.wilBitGradientPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.wilBitGradientPanel1.ResumeLayout(false);
+            this.wilBitGradientPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private WiLBiT.WiLBiTGradientPanel wilBitGradientPanel1;
+        private Label label1;
+        private Panel painel;
+
+
     }
 }
