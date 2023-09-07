c6715bab62944f1bccb89fa59069cc04b630fbb6
VICTOR GABRIEL DOS SANTOS SILVA <victor.gssilva9@senacsp.edu.br>
2023-09-07 16:09:23 -0300

    Programei o botao de login

--- pi-serasa-greenloop/Login.cs
+++ pi-serasa-greenloop/Login.cs
@@ -10,112 +10,112 @@ using System.Windows.Forms;
 
 namespace pi_serasa_greenloop
 {
-    public partial class Login : Form
-    {
-        public Login()
-        {
-            InitializeComponent();
-        }
+	public partial class Login : Form
+	{
+		public Login()
+		{
+			InitializeComponent();
+		}
 
 
-        void adicionarUsuario()
-        {
-            string nome = txtCadastroNome.Texts;
-            string senha = txtCadastroSenha.Texts;
-            string cpf = txtCadastroCPF.Texts;
-            string email = txtCadastroEmail.Texts;
-            string idade = txtIdade.Texts;
+		void adicionarUsuario()
+		{
+			string nome = txtCadastroNome.Texts;
+			string senha = txtCadastroSenha.Texts;
+			string cpf = txtCadastroCPF.Texts;
+			string email = txtCadastroEmail.Texts;
+			string idade = txtIdade.Texts;
 
-            Pessoas pessoas = new Pessoas(nome, idade, email, senha, cpf);
+			Pessoas pessoas = new Pessoas( nome, idade, email, senha, cpf);
 
-            pessoas.adicionarUsuario();
-        }
+			pessoas.adicionarUsuario();
+		}
 
-        void verificaCampoCadastro()
-        {
-            bool camposVazios = false;
+		void verificaCampoCadastro()
+		{
+			bool camposVazios = false;
 
-            switch (txtCadastroNome.Texts)
-            {
-                case "":
-                    camposVazios = true;
-                    break;
-            }
+			switch (txtCadastroNome.Texts)
+			{
+				case "":
+					camposVazios = true;
+					break;
+			}
 
-            switch (txtCadastroSenha.Texts)
-            {
-                case "":
-                    camposVazios = true;
-                    break;
-            }
+			switch (txtCadastroSenha.Texts)
+			{
+				case "":
+					camposVazios = true;
+					break;
+			}
 
-            switch (txtCadastroCPF.Texts)
-            {
-                case "":
-                    camposVazios = true;
-                    break;
-            }
+			switch (txtCadastroCPF.Texts)
+			{
+				case "":
+					camposVazios = true;
+					break;
+			}
 
-            switch (txtCadastroEmail.Texts)
-            {
-                case "":
-                    camposVazios = true;
-                    break;
-            }
+			switch (txtCadastroEmail.Texts)
+			{
+				case "":
+					camposVazios = true;
+					break;
+			}
 
-            switch (txtIdade.Texts)
-            {
-                case "":
-                    camposVazios = true;
-                    break;
-            }
+			switch (txtIdade.Texts)
+			{
+				case "":
+					camposVazios = true;
+					break;
+			}
 
 
-            if (camposVazios)
-            {
-                MessageBox.Show("Preencha todos os campos antes de continuar.", "Campos Vazios", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return;
-            }
+			if (camposVazios)
+			{
+				MessageBox.Show("Preencha todos os campos antes de continuar.", "Campos Vazios", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				return;
+			}
 
-            else
-            {
-                adicionarUsuario();
-                MessageBox.Show("Usuario Cadastrado!");
-            }
-        }
+			else
+			{
+				adicionarUsuario();
+				MessageBox.Show("Usuario Cadastrado!");
+			}
+		}
 
-        void login()
-        {
-            btnLoginn.BackColor = Color.Lime;
-            btnCadastroo.BackColor = Color.Gray;
+		void login()
+		{
+			btnLoginn.BackColor = Color.Lime;
+			btnCadastroo.BackColor = Color.Gray;
 
-            btnLoginn.ForeColor = Color.Black;
-            btnCadastroo.ForeColor = Color.White;
+			btnLoginn.ForeColor = Color.Black;
+			btnCadastroo.ForeColor = Color.White;
 
-            wilBitPanel2.Visible = false;
-            wilBitPanel1.Visible = true;
+			wilBitPanel2.Visible = false;
+			wilBitPanel1.Visible = true;
 
-            int centerX = (this.ClientSize.Width - wilBitPanel1.Width - wilBitPanel2.Width) / 2;
-            int centerY = (this.ClientSize.Height - wilBitPanel1.Height - 115) / 2;
-            btnLoginn.Location = new Point(centerX, centerY);
+			int centerX = (this.ClientSize.Width - wilBitPanel1.Width - wilBitPanel2.Width) / 2;
+			int centerY = (this.ClientSize.Height - wilBitPanel1.Height - 115) / 2;
+			btnLoginn.Location = new Point(centerX, centerY);
 
-            int centerXx = (this.ClientSize.Width + 666 + 30) / 2;
-            int centerYy = (this.ClientSize.Height - wilBitPanel2.Height - 115) / 2;
-            btnCadastroo.Location = new Point(centerXx, centerYy);
-        }
+			int centerXx = (this.ClientSize.Width + 666 + 30) / 2;
+			int centerYy = (this.ClientSize.Height - wilBitPanel2.Height - 115) / 2;
+			btnCadastroo.Location = new Point(centerXx, centerYy);
+		}
 
-        void cadastro()
-        {
-            btnCadastroo.BackColor = Color.Lime;
-            btnLoginn.BackColor = Color.Gray;
+		void cadastro()
+		{
+			btnCadastroo.BackColor = Color.Lime;
+			btnLoginn.BackColor = Color.Gray;
 
-            btnCadastroo.ForeColor = Color.Black;
-            btnLoginn.ForeColor = Color.White;
+			btnCadastroo.ForeColor = Color.Black;
+			btnLoginn.ForeColor = Color.White;
 
-            wilBitPanel1.Visible = false;
-            wilBitPanel2.Visible = true;
+			wilBitPanel1.Visible = false;
+			wilBitPanel2.Visible = true;
 
-        }
+		}
 
 		public void carregaForm(Form form)
 		{
@@ -129,13 +129,13 @@ namespace pi_serasa_greenloop
 		}
 
 		private void wilBitPanel2_Paint(object sender, PaintEventArgs e)
-        {
+		{
 
-        }
+		}
 
-        //CADASTRO
+		//CADASTRO
 
-        /*
+		/*
         private void wilBitButton1_Click(object sender, EventArgs e)
         {
             adicionarUsuario();
@@ -143,80 +143,99 @@ namespace pi_serasa_greenloop
         }
         */
 
-        private void wilBitPanel1_Paint(object sender, PaintEventArgs e)
-        {
+		private void wilBitPanel1_Paint(object sender, PaintEventArgs e)
+		{
 
-        }
+		}
 
-        private void Login_Load(object sender, EventArgs e)
-        {
-            int centerX = (this.ClientSize.Width - wilBitPanel1.Width - wilBitPanel2.Width) / 2;
-            int centerY = (this.ClientSize.Height - wilBitPanel1.Height) / 2;
-            wilBitPanel1.Location = new Point(centerX, centerY);
-            wilBitPanel2.Location = new Point(centerX + wilBitPanel1.Width, centerY);
-        }
+		private void Login_Load(object sender, EventArgs e)
+		{
+			int centerX = (this.ClientSize.Width - wilBitPanel1.Width - wilBitPanel2.Width) / 2;
+			int centerY = (this.ClientSize.Height - wilBitPanel1.Height) / 2;
+			wilBitPanel1.Location = new Point(centerX, centerY);
+			wilBitPanel2.Location = new Point(centerX + wilBitPanel1.Width, centerY);
+		}
 
-        private void txtCadastroNome__TextChanged(object sender, EventArgs e)
-        {
+		private void txtCadastroNome__TextChanged(object sender, EventArgs e)
+		{
 
-        }
+		}
 
-        private void Login_Load_1(object sender, EventArgs e)
-        {
-            int centerX = (this.ClientSize.Width - wilBitPanel1.Width - wilBitPanel2.Width) / 2;
-            int centerY = (this.ClientSize.Height - wilBitPanel1.Height) / 2;
-            wilBitPanel1.Location = new Point(centerX, centerY);
-            wilBitPanel2.Location = new Point(centerX + wilBitPanel1.Width, centerY);
+		private void Login_Load_1(object sender, EventArgs e)
+		{
+			int centerX = (this.ClientSize.Width - wilBitPanel1.Width - wilBitPanel2.Width) / 2;
+			int centerY = (this.ClientSize.Height - wilBitPanel1.Height) / 2;
+			wilBitPanel1.Location = new Point(centerX, centerY);
+			wilBitPanel2.Location = new Point(centerX + wilBitPanel1.Width, centerY);
 
-            login();
-        }
+			login();
+		}
 
-        private void wilBitButton1_Click_1(object sender, EventArgs e)
-        {
-            verificaCampoCadastro();
-        }
+		private void wilBitButton1_Click_1(object sender, EventArgs e)
+		{
+			verificaCampoCadastro();
+		}
 
-        private void btnLoginn_Click(object sender, EventArgs e)
-        {
-            login();
-        }
+		private void btnLoginn_Click(object sender, EventArgs e)
+		{
+			login();
+		}
 
-        private void btnCadastroo_Click(object sender, EventArgs e)
-        {
-            cadastro();
-        }
+		private void btnCadastroo_Click(object sender, EventArgs e)
+		{
+			cadastro();
+		}
 
-        private void txtCadastroSenha__TextChanged(object sender, EventArgs e)
-        {
+		private void txtCadastroSenha__TextChanged(object sender, EventArgs e)
+		{
 
-        }
+		}
 
-        private void btnLoginn_Click_1(object sender, EventArgs e)
-        {
-            login();
-        }
+		private void btnLoginn_Click_1(object sender, EventArgs e)
+		{
+			login();
+		}
 
-        private void btnCadastroo_Click_1(object sender, EventArgs e)
-        {
-            cadastro();
-        }
+		private void btnCadastroo_Click_1(object sender, EventArgs e)
+		{
+			cadastro();
+		}
 
-        private void Login_Load_2(object sender, EventArgs e)
-        {
-            int centerX = (this.ClientSize.Width - wilBitPanel1.Width - wilBitPanel2.Width) / 2;
-            int centerY = (this.ClientSize.Height - wilBitPanel1.Height) / 2;
-            wilBitPanel1.Location = new Point(centerX, centerY);
-            wilBitPanel2.Location = new Point(centerX + wilBitPanel1.Width, centerY);
+		private void Login_Load_2(object sender, EventArgs e)
+		{
+			int centerX = (this.ClientSize.Width - wilBitPanel1.Width - wilBitPanel2.Width) / 2;
+			int centerY = (this.ClientSize.Height - wilBitPanel1.Height) / 2;
+			wilBitPanel1.Location = new Point(centerX, centerY);
+			wilBitPanel2.Location = new Point(centerX + wilBitPanel1.Width, centerY);
 
-            login();
-        }
+			login();
+		}
 
-        private void wilBitButton1_Click(object sender, EventArgs e)
-        {
-            verificaCampoCadastro();
-            adicionarUsuario();
-            carregaForm(new Principal());
-            Form1.btnVoltar.Visible = true;
-        }
-    }
+		private void wilBitButton1_Click(object sender, EventArgs e)
+		{
+			verificaCampoCadastro();
+			adicionarUsuario();
+			carregaForm(new Principal());
+			Form1.btnVoltar.Visible = true;
+		}
+
+		private void btnLogin_Click(object sender, EventArgs e)
+		{
+			string email = txtLoginEmail.Texts;
+			string senha = txtLoginSenha.Texts;
+
+			Pessoas pessoas = new Pessoas();
+			pessoas = pessoas.login(email, senha);
+			if (pessoas == null)
+			{
+				MessageBox.Show("Usuário ou senha incorreto");
+			}
+			else
+			{
+				MessageBox.Show("Usuário encontrado");
+			}
+
+			carregaForm(new Principal());
+		}
+	}
 }
--- pi-serasa-greenloop/Pessoas.cs
+++ pi-serasa-greenloop/Pessoas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,16 @@ namespace pi_serasa_greenloop
 {
     internal class Pessoas
     {
-        string nome;
-        string idade;
-        string email;
-        string senha;
-        string cpf;
+        public int Id;
+        public string nome;
+        public string idade;
+        public string email;
+        public string senha;
+        public string cpf;
 
         public Pessoas(string nome, string idade, string email, string senha, string cpf) 
         {
+            
             this.nome = nome;
             this.idade = idade;
             this.email = email;
@@ -32,5 +35,32 @@ namespace pi_serasa_greenloop
             string query = $"INSERT INTO pessoas(nome, idade, email, senha, cpf) VALUES ('{nome}', '{idade}', '{email}', '{senha}', {cpf})";
             Conexao.executaQuery(query);
         }
+
+        public Pessoas login(string email, string senha)
+        {
+			string query = $"SELECT * FROM pessoas WHERE email = '{email}' AND senha = '{senha}'";
+
+            DataTable resultados = Conexao.executaQuery(query);
+            if(resultados.Rows.Count == 0)
+            {
+                return null;
+            }
+
+            Pessoas pessoa = carregaDados(resultados.Rows[0]);
+            return pessoa;
+		}
+
+        private Pessoas carregaDados(DataRow row)
+        {
+            
+			string nome = row["id"].ToString();
+            string idade = row["idade"].ToString();
+            string email = row["email"].ToString();
+			string senha = row["senha"].ToString();
+			string cpf = row["cpf"].ToString();
+
+            Pessoas pessoas = new Pessoas(nome, idade, email, senha, cpf);
+            return pessoas;
+		}
     }
 }
