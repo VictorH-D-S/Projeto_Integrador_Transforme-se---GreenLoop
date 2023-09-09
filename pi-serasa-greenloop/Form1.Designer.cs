2d7f43b8c1ccf318b270baba7fc5439951fc8860
Victor H Santos <victorhds0112@gmail.com>
2023-09-08 23:51:52 -0300

    NOVA CORREÇÃO DE TEXTO DO FORM1
    Ignorar o revert (reverti a cagada que tinha dado)

--- pi-serasa-greenloop/Form1.Designer.cs
+++ pi-serasa-greenloop/Form1.Designer.cs
@@ -1,114 +1,115 @@
 ﻿namespace pi_serasa_greenloop
 {
-	partial class Form1
-	{
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		private System.ComponentModel.IContainer components = null;
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && (components != null))
-			{
-				components.Dispose();
-			}
-			base.Dispose(disposing);
-		}
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		public void InitializeComponent()
-		{
-			btnVoltar = new WiLBiT.WiLBiTButton();
-			painel = new Panel();
-			panel1 = new Panel();
-			label2 = new Label();
-			panel1.SuspendLayout();
-			SuspendLayout();
-			// 
-			// btnVoltar
-			// 
-			btnVoltar.BackColor = Color.FromArgb(255, 128, 0);
-			btnVoltar.BorderColor = Color.FromArgb(72, 116, 245);
-			btnVoltar.BorderRadius = 6;
-			btnVoltar.BorderSize = 0;
-			btnVoltar.FlatAppearance.BorderSize = 0;
-			btnVoltar.FlatStyle = FlatStyle.Flat;
-			btnVoltar.Font = new Font("Trebuchet MS", 12F, FontStyle.Bold, GraphicsUnit.Point);
-			btnVoltar.ForeColor = Color.White;
-			btnVoltar.Location = new Point(61, 23);
-			btnVoltar.Name = "btnVoltar";
-			btnVoltar.Size = new Size(125, 47);
-			btnVoltar.TabIndex = 1;
-			btnVoltar.Text = "Voltar";
-			btnVoltar.UseVisualStyleBackColor = false;
-			btnVoltar.Click += btnVoltar_Click;
-			// 
-			// painel
-			// 
-			painel.Anchor = AnchorStyles.None;
-			painel.Location = new Point(0, 78);
-			painel.Name = "painel";
-			painel.Size = new Size(1451, 786);
-			painel.TabIndex = 1;
-			painel.Paint += painel_Paint;
-			// 
-			// panel1
-			// 
-			panel1.Anchor = AnchorStyles.Left | AnchorStyles.Right;
-			panel1.BackColor = Color.LimeGreen;
-			panel1.Controls.Add(btnVoltar);
-			panel1.Controls.Add(label2);
-			panel1.ForeColor = Color.LimeGreen;
-			panel1.Location = new Point(-6, -11);
-			panel1.Name = "panel1";
-			panel1.Size = new Size(1456, 89);
-			panel1.TabIndex = 2;
-			// 
-			// label2
-			// 
-			label2.Anchor = AnchorStyles.Left | AnchorStyles.Right;
-			label2.AutoSize = true;
-			label2.Font = new Font("Trebuchet MS", 24F, FontStyle.Bold, GraphicsUnit.Point);
-			label2.ForeColor = Color.White;
-			label2.Location = new Point(1101, 30);
-			label2.Name = "label2";
-			label2.Size = new Size(180, 40);
-			label2.TabIndex = 0;
-			label2.Text = "GreenLoop";
-			label2.Click += label2_Click;
-			// 
-			// Form1
-			// 
-			AutoScaleDimensions = new SizeF(7F, 15F);
-			AutoScaleMode = AutoScaleMode.Font;
-			BackColor = Color.FromArgb(0, 3, 21);
-			ClientSize = new Size(1444, 866);
-			Controls.Add(panel1);
-			Controls.Add(painel);
-			Name = "Form1";
-			Text = "Form1";
-			Load += Form1_Load;
-			panel1.ResumeLayout(false);
-			panel1.PerformLayout();
-			ResumeLayout(false);
-		}
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        public void InitializeComponent()
+        {
+            btnVoltar = new WiLBiT.WiLBiTButton();
+            painel = new Panel();
+            panel1 = new Panel();
+            label2 = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnVoltar
+            // 
+            btnVoltar.BackColor = Color.FromArgb(255, 128, 0);
+            btnVoltar.BorderColor = Color.FromArgb(72, 116, 245);
+            btnVoltar.BorderRadius = 6;
+            btnVoltar.BorderSize = 0;
+            btnVoltar.FlatAppearance.BorderSize = 0;
+            btnVoltar.FlatStyle = FlatStyle.Flat;
+            btnVoltar.Font = new Font("Trebuchet MS", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnVoltar.ForeColor = Color.White;
+            btnVoltar.Location = new Point(61, 23);
+            btnVoltar.Name = "btnVoltar";
+            btnVoltar.Size = new Size(125, 47);
+            btnVoltar.TabIndex = 1;
+            btnVoltar.Text = "Voltar";
+            btnVoltar.UseVisualStyleBackColor = false;
+            btnVoltar.Click += btnVoltar_Click;
+            // 
+            // painel
+            // 
+            painel.Anchor = AnchorStyles.None;
+            painel.Location = new Point(0, 78);
+            painel.Name = "painel";
+            painel.Size = new Size(1451, 786);
+            painel.TabIndex = 1;
+            painel.Paint += painel_Paint;
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            panel1.BackColor = Color.LimeGreen;
+            panel1.Controls.Add(btnVoltar);
+            panel1.Controls.Add(label2);
+            panel1.ForeColor = Color.LimeGreen;
+            panel1.Location = new Point(-6, -11);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1456, 89);
+            panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.None;
+            label2.AutoSize = true;
+            label2.Font = new Font("Trebuchet MS", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(1101, 30);
+            label2.Name = "label2";
+            label2.Size = new Size(180, 40);
+            label2.TabIndex = 0;
+            label2.Text = "GreenLoop";
+            label2.TextAlign = ContentAlignment.TopCenter;
+            label2.Click += label2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(0, 3, 21);
+            ClientSize = new Size(1444, 866);
+            Controls.Add(panel1);
+            Controls.Add(painel);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
 
-		public WiLBiT.WiLBiTButton btnVoltar;
+        public WiLBiT.WiLBiTButton btnVoltar;
 
-		#endregion
+        #endregion
 
-		public Label label2;
-		public Panel painel;
-		public Panel panel1;
-	}
+        public static Label label2;
+        public static Panel painel;
+        public static Panel panel1;
+    }
 }
--- pi-serasa-greenloop/Form1.cs
+++ pi-serasa-greenloop/Form1.cs
@@ -10,13 +10,13 @@ using System.Windows.Forms;
 
 namespace pi_serasa_greenloop
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-		/*private void painel_SizeChanged(object sender, EventArgs e)
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /*private void painel_SizeChanged(object sender, EventArgs e)
         {
             // Centraliza o formulário dentro do painel
             int formX = (painel.Width - this.Width) / 2;
@@ -25,67 +25,67 @@ namespace pi_serasa_greenloop
             this.Location = new System.Drawing.Point(formX, formY);
         }
         */
-		void responsivo()
-		{
-			//Centraliazdor de painel do governo do amor
-			int formLARGURA = this.Width, formALTURA = this.Height, panelLARGURA = panel1.Width, panelALTURA = panel1.Height;
-			int centralizaPainel = (formLARGURA - panel1.Width) / 2;
-			panel1.Location = new Point(centralizaPainel, 0);
+        void responsivo()
+        {
+            //Centraliazdor de painel do governo do amor
+            int formLARGURA = this.Width, formALTURA = this.Height, panelLARGURA = panel1.Width, panelALTURA = panel1.Height;
+            int centralizaPainel = (formLARGURA - panel1.Width) / 2;
+            panel1.Location = new Point(centralizaPainel, 0);
 
-			//Centralizador de label termine minha vida
-			int labelLargura = (panel1.Width - label2.Width) / 2;
-			int LabelAltura = (panel1.Height - label2.Height) / 2;
-			label2.Location = new Point(labelLargura, LabelAltura);
+            //Centralizador de label termine minha vida
+            int labelLargura = (panel1.Width - label2.Width) / 2;
+            int LabelAltura = (panel1.Height - label2.Height) / 2;
+            label2.Location = new Point(labelLargura, LabelAltura);
 
-			//Centralizador de painéis do shadow o ouriço(este aqui deixa os painéis responsivos quando carregamos no form1 HAHAHAHAHAHA)
-			int formX = (painel.Width - this.Width) / 2;
-			int formY = (painel.Height - this.Height) / 2;
-			this.Location = new Point(formX, formY);
+            //Centralizador de painéis do shadow o ouriço(este aqui deixa os painéis responsivos quando carregamos no form1 HAHAHAHAHAHA)
+            int formX = (painel.Width - this.Width) / 2;
+            int formY = (painel.Height - this.Height) / 2;
+            this.Location = new Point(formX, formY);
 
-			WindowState = FormWindowState.Maximized;
-			panel1.Dock = DockStyle.Top;
-			btnVoltar.Anchor = AnchorStyles.Left;
-			label2.Anchor = AnchorStyles.None;
-			label2.TextAlign = ContentAlignment.MiddleCenter;
-			btnVoltar.Visible = false;
+            WindowState = FormWindowState.Maximized;
+            panel1.Dock = DockStyle.Top;
+            btnVoltar.Anchor = AnchorStyles.Left;
+            label2.Anchor = AnchorStyles.None;
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            btnVoltar.Visible = false;
 
-		}
+        }
 
-		public void carregaForm(Form form)
-		{
-			form.TopLevel = false;
-			painel.Controls.Clear();
-			painel.Controls.Add(form);
-			form.Size = painel.Size;
-			form.Location = new Point(painel.Width - form.Width, painel.Height - form.Height);
-			form.Show();
+        public void carregaForm(Form form)
+        {
+            form.TopLevel = false;
+            painel.Controls.Clear();
+            painel.Controls.Add(form);
+            form.Size = painel.Size;
+            form.Location = new Point(painel.Width - form.Width, painel.Height - form.Height);
+            form.Show();
 
-		}
+        }
 
-		private void Form1_Load(object sender, EventArgs e)
-		{
-			responsivo();
-			carregaForm(new Login());
-			//painel_SizeChanged(null, null);
-		}
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            responsivo();
+            carregaForm(new Login());
+            //painel_SizeChanged(null, null);
+        }
 
-		private void btnVoltar_Click(object sender, EventArgs e)
-		{
-			painel.Controls.Clear();
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            painel.Controls.Clear();
 
-			carregaForm(new Principal());
-		}
+            carregaForm(new Principal());
+        }
 
-		private void painel_Paint(object sender, PaintEventArgs e)
-		{
+        private void painel_Paint(object sender, PaintEventArgs e)
+        {
 
-		}
+        }
 
-		private void label2_Click(object sender, EventArgs e)
-		{
+        private void label2_Click(object sender, EventArgs e)
+        {
 
-		}
-	}
+        }
+    }
 }
 
 /*
