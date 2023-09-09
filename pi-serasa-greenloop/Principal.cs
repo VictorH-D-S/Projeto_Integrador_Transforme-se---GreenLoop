c49eaccfabef72395448fda8358220118698bb65
VICTOR GABRIEL DOS SANTOS SILVA <victor.gssilva9@senacsp.edu.br>
2023-09-08 23:35:51 -0300

    Arrumei os botoes da home

--- pi-serasa-greenloop/Principal.cs
+++ pi-serasa-greenloop/Principal.cs
@@ -10,119 +10,129 @@ using System.Windows.Forms;
 
 namespace pi_serasa_greenloop
 {
-    public partial class Principal : Form
-    {
-        public Principal()
-        {
-            InitializeComponent();
-        }
+	public partial class Principal : Form
+	{
+		public Principal()
+		{
+			InitializeComponent();
+		}
 
-        Label lblPrincipal = new Label();
-
-
-        public void carregaForm(Form form)
-        {
-
-            form.TopLevel = false;
-            painel.Controls.Clear();
-            painel.Controls.Add(form);
-            form.Size = painel.Size;
-            form.Location = new Point(painel.Width - form.Width, painel.Height - form.Height);
-            form.Show();
-
-        }
-
-        public void btnRecompensas_Click(object sender, EventArgs e)
-        {
+		Label lblPrincipal = new Label();
 
 
-            recompensas recompensas = new recompensas();
-            recompensas.Show();
-        }
+		public void carregaForm(Form form)
+		{
 
-        private void btnPerfil_Click(object sender, EventArgs e)
-        {
+			form.TopLevel = false;
+			painel.Controls.Clear();
+			painel.Controls.Add(form);
+			form.Size = painel.Size;
+			form.Location = new Point(painel.Width - form.Width, painel.Height - form.Height);
+			form.Show();
 
-        }
+		}
 
-        private void Principal_Load(object sender, EventArgs e)
-        {
+		public void btnRecompensas_Click(object sender, EventArgs e)
+		{
 
-        }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
+			recompensas recompensas = new recompensas();
+			recompensas.Show();
+		}
 
-        }
+		private void btnPerfil_Click(object sender, EventArgs e)
+		{
 
-        private void btnPontosColeta_Click(object sender, EventArgs e)
-        {
+		}
 
-        }
+		private void Principal_Load(object sender, EventArgs e)
+		{
 
-        private void label7_Click(object sender, EventArgs e)
-        {
+		}
 
-        }
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
 
-        private void label6_Click(object sender, EventArgs e)
-        {
+		}
 
-        }
+		private void btnPontosColeta_Click(object sender, EventArgs e)
+		{
 
-        private void label9_Click(object sender, EventArgs e)
-        {
+		}
 
-        }
+		private void label7_Click(object sender, EventArgs e)
+		{
 
-        private void label8_Click(object sender, EventArgs e)
-        {
+		}
 
-        }
+		private void label6_Click(object sender, EventArgs e)
+		{
 
-        private void btnAprender_Click(object sender, EventArgs e)
-        {
+		}
 
-        }
+		private void label9_Click(object sender, EventArgs e)
+		{
 
-        private void label5_Click(object sender, EventArgs e)
-        {
+		}
 
-        }
+		private void label8_Click(object sender, EventArgs e)
+		{
 
-        private void label4_Click(object sender, EventArgs e)
-        {
+		}
 
-        }
+		private void btnAprender_Click(object sender, EventArgs e)
+		{
 
-        private void btnPerfil_Click_1(object sender, EventArgs e)
-        {
-            carregaForm(new Perfil());
-        }
+		}
 
-        private void btnRecompensas_Click_1(object sender, EventArgs e)
-        {
+		private void label5_Click(object sender, EventArgs e)
+		{
 
-            carregaForm(new recompensas());
-        }
+		}
 
-        private void iconPictureBox1_Click(object sender, EventArgs e)
-        {
-        }
+		private void label4_Click(object sender, EventArgs e)
+		{
 
-        private void pictureBox1_Click_1(object sender, EventArgs e)
-        {
+		}
 
-        }
+		private void btnPerfil_Click_1(object sender, EventArgs e)
+		{
+			carregaForm(new Perfil());
+		}
 
-        private void btnReciclagem_Click(object sender, EventArgs e)
-        {
+		private void btnRecompensas_Click_1(object sender, EventArgs e)
+		{
 
-        }
+			carregaForm(new recompensas());
+		}
 
-        private void painel_Paint(object sender, PaintEventArgs e)
-        {
+		private void iconPictureBox1_Click(object sender, EventArgs e)
+		{
+		}
 
-        }
-    }
+		private void pictureBox1_Click_1(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnReciclagem_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void painel_Paint(object sender, PaintEventArgs e)
+		{
+
+		}
+
+		private void wilBitRoundedPictureBox22_Click(object sender, EventArgs e)
+		{
+			carregaForm(new Perfil());
+		}
+
+		private void wilBitRoundedPictureBox21_Click(object sender, EventArgs e)
+		{
+			carregaForm(new recompensas());
+		}
+	}
 }
