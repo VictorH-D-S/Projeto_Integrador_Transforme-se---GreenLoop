c3480732e222f2b2c55ebfa78bc6faab921ee504
Victor H Santos <victorhds0112@gmail.com>
2023-09-13 02:02:46 -0300

    Teste Responsividade

--- pi-serasa-greenloop/recompensas.cs
+++ pi-serasa-greenloop/recompensas.cs
@@ -21,8 +21,8 @@ namespace pi_serasa_greenloop
         {
             painelConteudo.Location = new Point((this.ClientSize.Width - painelConteudo.Width) / 2, (this.ClientSize.Height - painelConteudo.Height + 140) / 2);
             this.WindowState = FormWindowState.Maximized;
-            
-            
+
+
             pnlMenuCima.Location = new Point(0, 0);
 
         }
@@ -44,13 +44,43 @@ namespace pi_serasa_greenloop
 
         private void recompensas_Load_1(object sender, EventArgs e)
         {
-            Form1 form = new Form1();
-            painelConteudo.Location = new Point((this.ClientSize.Width - painelConteudo.Width) / 2, (this.ClientSize.Height - painelConteudo.Height + 140) / 2);
-            this.WindowState = FormWindowState.Maximized;
+            AtualizarPosicionamento();
 
+            // Registre o evento SizeChanged para ajustar o layout quando o tamanho da janela for alterado.
+            this.SizeChanged += new EventHandler(recompensas_SizeChanged);
+        }
 
-            pnlMenuCima.Location = new Point(0, 0);
-            form.responsivo();
+        private void AtualizarPosicionamento()
+        {
+            int centerX = (this.ClientSize.Width - painelConteudo.Width) / 2;
+            int centerY = (this.ClientSize.Height - painelConteudo.Height + 140) / 2;
+
+            // Posicione o painel de conteúdo no centro da janela
+            painelConteudo.Location = new Point(centerX, centerY);
+
+            // Centralize o painel de menu superior no topo da janela horizontalmente
+            int menuTopX = (this.ClientSize.Width - pnlMenuCima.Width) / 2;
+            pnlMenuCima.Location = new Point(menuTopX, 0);
+        }
+
+        private void recompensas_SizeChanged(object sender, EventArgs e)
+        {
+            // Quando o tamanho da janela for alterado, atualize o posicionamento.
+            AtualizarPosicionamento();
+        }
+
+        private void lblPontos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void wilBitPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
